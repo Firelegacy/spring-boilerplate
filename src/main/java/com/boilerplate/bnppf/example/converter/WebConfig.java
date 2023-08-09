aa355c7d225675259212b89a087cf518f28f9f2e
firelegacy <v.dabrowski@outlook.com>
2023-08-10 01:48:10 +0200

    added converter doc

--- src/main/java/com/boilerplate/bnppf/example/converter/WebConfig.java
+++ src/main/java/com/boilerplate/bnppf/example/converter/WebConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /* Makes these converters available through the Spring object ConversionService (autowired)
+    * use like in this example: Demand demand = conversionService.convert(demandDetails, Demand.class);
+    * */
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new DemandDetailsToDemandConverter());
