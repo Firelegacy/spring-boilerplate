578c8917072fbc0101df3e9535fd42e1d4b31cd7
firelegacy <v.dabrowski@outlook.com>
2023-08-10 02:04:19 +0200

    Add validation

--- src/main/java/com/boilerplate/bnppf/example/model/DemandDetails.java
+++ src/main/java/com/boilerplate/bnppf/example/model/DemandDetails.java
@@ -2,25 +2,40 @@ package com.boilerplate.bnppf.example.model;
 
 import com.boilerplate.bnppf.example.enums.CivilStatus;
 import com.boilerplate.bnppf.example.enums.MaritalStatus;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
+/* Validation examples https://www.baeldung.com/java-validation#:~:text=%40NotNull%20validates%20that%20the%20annotated,%2C%20Map%2C%20and%20array%20properties. */
 @Builder
 @Data
 public class DemandDetails {
+    @NotBlank
     private String productId;
 
+    @Size(min = 2)
     private String firstname;
 
+    @Size(min = 2)
     private String lastname;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @NotNull
+    @Past
     private LocalDate birthdate;
 
+    @NotNull
     private CivilStatus civilStatus;
 
     private MaritalStatus maritalStatus;
+
+    @AssertTrue
+    private boolean isAcceptedMaritalStatus() {
+        if (civilStatus.equals(CivilStatus.MARRIED)) {
+            return !maritalStatus.equals(MaritalStatus.SEPERATED_ESTATE) && !maritalStatus.equals(MaritalStatus.UNIVERSAL_COMMUNITY);
+        } else {
+            return true;
+        }
+    }
 }
